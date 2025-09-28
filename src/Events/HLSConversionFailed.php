093d6a03fe5209327cd9796324ed0a5ad39986b9
achyutkneupane <achyutkneupane@gmail.com>
2025-09-28 13:36:48 +0000

    feat: adds HLSConversionFailed event for failed HLS conversion

---
+++ src/Events/HLSConversionFailed.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AchyutN\LaravelHLS\Events;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Event fired when HLS conversion fails.
+ */
+final class HLSConversionFailed
+{
+    use Dispatchable, SerializesModels;
+
+    public function __construct(
+        public readonly Model $model,
+    ) {
+    }
+}
